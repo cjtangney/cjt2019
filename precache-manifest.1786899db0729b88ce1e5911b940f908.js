47eecfdd9bfe43f06efc720968722d33409aab66
Connor Tangney <Couch3ater@gmail.com>
2019-01-26 08:18:18 -0500

    Updates

---
+++ precache-manifest.1786899db0729b88ce1e5911b940f908.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d985881f24928c78835",
+    "url": "/cjt2019/static/js/runtime~main.4d985881.js"
+  },
+  {
+    "revision": "5299aea8e3bde710f869",
+    "url": "/cjt2019/static/js/main.5299aea8.chunk.js"
+  },
+  {
+    "revision": "e163c4f92ed13ff7c44a",
+    "url": "/cjt2019/static/js/1.e163c4f9.chunk.js"
+  },
+  {
+    "revision": "5299aea8e3bde710f869",
+    "url": "/cjt2019/static/css/main.f6d1b2b5.chunk.css"
+  },
+  {
+    "revision": "fbe30133259402fe24e51353406ae7df",
+    "url": "/cjt2019/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.99d35c7c9ab134295938418c1b6ef0cb.js"
+  "/cjt2019/precache-manifest.1786899db0729b88ce1e5911b940f908.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-gh-pages/index.html", {
+workbox.routing.registerNavigationRoute("/cjt2019/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
